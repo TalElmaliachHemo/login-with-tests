1f2b093f7e6d36a0facf6060605ac2eadf81741f
Tal <tale1000@gmail.com>
2022-11-28 17:10:43 +0200

    fix bug in the user reducer test file

--- src/store/reducers/user.reducer.test.js
+++ src/store/reducers/user.reducer.test.js
@@ -5,7 +5,7 @@ describe('user reducer', () => {
     const mockUser = {username: 'roni', email: 'roni123@gmail.com', id: 'i101'}
     const initialState = {
         user: null,
-        error: null
+        errorMsg: null
     }
 
     it('creates initial state', async () => {
@@ -19,16 +19,16 @@ describe('user reducer', () => {
 
         state = userReducer(initialState, {type: 'SET_USER', user: mockUser})
         expect(state.user).toBeTruthy()
-        expect(state.error).toBeFalsy()
+        expect(state.errorMsg).toBeFalsy()
     })
 
     it('should set an error message in the state', async () => {
         let state = userReducer(initialState)
-        expect(state.error).toBeFalsy()
+        expect(state.errorMsg).toBeFalsy()
 
         state = userReducer(initialState, {type: 'SET_ERROR', error: 'an error'})
-        expect(state.error).toBeTruthy()
-        expect(state.error).toBe('an error')
+        expect(state.errorMsg).toBeTruthy()
+        expect(state.errorMsg).toBe('an error')
         expect(state.user).toBeFalsy()
     })
 })
